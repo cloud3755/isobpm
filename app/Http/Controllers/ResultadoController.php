3dee7e1a0883bc5e3573e37e31680041d910f5ea
Unknown <arturodlt83@gmail.com>
2017-06-08 13:16:21 -0500

    se coloca filtro

--- app/Http/Controllers/ResultadoController.php
+++ app/Http/Controllers/ResultadoController.php
@@ -40,15 +40,33 @@ class ResultadoController extends Controller
     {
       $usuarios = Auth::user();
 
-      $indicadorrelacion = \DB::table('indicadores')
-                               ->leftjoin('objetivos','indicadores.objetivo_id','=','objetivos.id')
-                               ->leftjoin('users','indicadores.usuario_responsable_id','=','users.id')
-                               ->leftjoin('frecuencias','indicadores.frecuencia_id','=','frecuencias.id')
-                               ->leftjoin('unidades','indicadores.unidad','=','unidades.id')
-                               ->leftjoin('logicas','indicadores.logica','=','logicas.id')
-                               ->select('indicadores.id as id','indicadores.objetivo_id as objetivo_id','objetivos.nombre as indicadoresobjetivo','indicadores.nombre as nombreindicador','indicadores.descripcion as descripcionindicador','users.usuario as userindicador','frecuencias.nombre as frecuenciaindicador','unidades.simbolo as simboloindicador','logicas.simbolo as logicaindicador','indicadores.meta as indicadormeta')
-                               ->where('objetivos.id_compania',$usuarios->id_compania)
-                               ->get();
+      if($usuarios->perfil ==4)
+      {
+        $indicadorrelacion = \DB::table('indicadores')
+                                 ->leftjoin('objetivos','indicadores.objetivo_id','=','objetivos.id')
+                                 ->leftjoin('users','indicadores.usuario_responsable_id','=','users.id')
+                                 ->leftjoin('frecuencias','indicadores.frecuencia_id','=','frecuencias.id')
+                                 ->leftjoin('unidades','indicadores.unidad','=','unidades.id')
+                                 ->leftjoin('logicas','indicadores.logica','=','logicas.id')
+                                 ->leftjoin('lista_accesos','indicadores.acceso','=','lista_accesos.id_indicador')
+                                 ->select('indicadores.id as id','indicadores.objetivo_id as objetivo_id','objetivos.nombre as indicadoresobjetivo','indicadores.nombre as nombreindicador','indicadores.descripcion as descripcionindicador','users.usuario as userindicador','frecuencias.nombre as frecuenciaindicador','unidades.simbolo as simboloindicador','logicas.simbolo as logicaindicador','indicadores.meta as indicadormeta')
+                                 ->where('lista_accesos.id_usuario',$usuarios->id)
+                                 ->orwhere('indicadores.creador_id',$usuarios->id)
+                                 ->get();
+      }
+
+      else
+      {
+        $indicadorrelacion = \DB::table('indicadores')
+                                 ->leftjoin('objetivos','indicadores.objetivo_id','=','objetivos.id')
+                                 ->leftjoin('users','indicadores.usuario_responsable_id','=','users.id')
+                                 ->leftjoin('frecuencias','indicadores.frecuencia_id','=','frecuencias.id')
+                                 ->leftjoin('unidades','indicadores.unidad','=','unidades.id')
+                                 ->leftjoin('logicas','indicadores.logica','=','logicas.id')
+                                 ->select('indicadores.id as id','indicadores.objetivo_id as objetivo_id','objetivos.nombre as indicadoresobjetivo','indicadores.nombre as nombreindicador','indicadores.descripcion as descripcionindicador','users.usuario as userindicador','frecuencias.nombre as frecuenciaindicador','unidades.simbolo as simboloindicador','logicas.simbolo as logicaindicador','indicadores.meta as indicadormeta')
+                                 ->where('objetivos.id_compania',$usuarios->id_compania)
+                                 ->get();
+      }
        //return(dd($indicadorrelacion));
       //para validar si trae los campos correctos
       //dd($resultado->all());
