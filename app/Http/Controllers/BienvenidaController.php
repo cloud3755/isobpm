60c4d00c8bb515d0e95e4719d0d317b961cd57bf
Unknown <inf.jonathan.gomez@gmail.com>
2017-09-12 02:01:38 -0500

    Falto un archivo de los cambios

--- app/Http/Controllers/BienvenidaController.php
+++ app/Http/Controllers/BienvenidaController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use App\Models\Objetivo;
 use App\Models\Indicadores;
 use App\Models\Quejas;
@@ -100,39 +101,172 @@ class BienvenidaController extends Controller
     {
       $usuarios = Auth::user();
 
-
-
 		  if($usuarios->perfil == 4)
 		  {
-            $mejorasid = \DB::table('mejoras')
+
+        $iduser = $usuarios->id;
+        //Proyectos de mejora
+                    $mejorasid = \DB::table('mejoras')
                    ->join('users','mejoras.responsable_id','=','users.id')
                    ->leftjoin('lista_accesos','mejoras.listaequipo','=','lista_accesos.id_indicador')
                    ->select('mejoras.id', 'mejoras.proyecto')
                    ->where('lista_accesos.id_usuario','=',$usuarios->id)
-                   ->orwhere('mejoras.creador_id',$usuarios->id)
+                   ->where('mejoras.estatus_id','!=',3)
+                   ->orwhere(function ($querys) use ($iduser) {
+                     $querys->where('mejoras.estatus_id','!=',3)
+                     ->where('mejoras.creador_id', '=', DB::raw("'".$iduser."'"));
+                   })
                    ->groupby('mejoras.id')
                    ->get();
-		  }
+                   //Fin de proyectos de mejora
 
-		  else{
+                   //Noticias
+                   $noticiasw = \DB::table('noticias')
+                   ->join('lista_noticias', 'noticias.id', '=', 'lista_noticias.id_noticia')
+                   ->select('noticias.*')
+                   ->where('noticias.id_empresa',$usuarios->id_compania)
+                   ->where('lista_noticias.id_area',$usuarios->id_area)
+                   ->whereMonth('fecha_creacion', '=', date('m'))
+                   ->get();
+                   //fin de noticias
+
+                   // Procesos
+
+                   $collection_one = \Illuminate\Support\Collection::make(DB::table('procesos')
+                   ->select('procesos.*')
+                   ->join('lista_envios', function($join) use ($iduser)
+                     {
+                         $join->on('procesos.lista_de_distribucion', '=', 'lista_envios.id_proceso');
+                         $join->on(function($query) use ($iduser)
+                         {
+                           $query->on('lista_envios.id_usuario', '=', DB::raw("'".$iduser."'"));
+                         });
+                     })
+                   ->select('procesos.*')
+                   ->get());
+
+                   $collection_two = \Illuminate\Support\Collection::make(DB::table('procesos')
+                   ->select('procesos.*')
+                   ->leftjoin('lista_envios', function($join) use ($iduser)
+                     {
+                         $join->on('procesos.lista_de_distribucion', '=', 'lista_envios.id_proceso');
+                         $join->on(function($query) use ($iduser)
+                         {
+                           $query->on('lista_envios.id_usuario', '=', DB::raw("'".$iduser."'"));
+                         });
+                     })
+                   ->whereNull('id_proceso')
+                   ->where('usuario_responsable_id',$usuarios->id)
+                   ->orwhere(function ($querys) use ($iduser) {
+                     $querys->whereNull('id_proceso')
+                     ->Where('Creador_id', '=', DB::raw("'".$iduser."'"));
+                   })
+                   ->get());
+
+                   $procesoa = new \Illuminate\Database\Eloquent\Collection;
+                   $procesoa = $collection_one->merge($collection_two);
+                   // Final de Procesos
+
+                   //Quejas
+
+                   $quejas = \Illuminate\Support\Collection::make(\DB::table('quejas')
+                   ->join('clientes','quejas.cliente_id','=','clientes.id')
+                   ->join('users','quejas.usuario_responsable_id','=','users.id')
+                   ->join('estatuses','quejas.estatus_id','=','estatuses.id')
+                   ->select('quejas.*','users.nombre as usernombre','clientes.nombre as clientenombre','estatuses.nombre as statusnombre')
+                   ->where('quejas.idcompañia','=',$usuarios->id_compania)
+                   ->where('quejas.usuario_responsable_id','=',$usuarios->id)
+                   ->where('quejas.estatus_id','!=',3)
+                   ->get());
+                   //Final de quejas
+
+                   //Acciones correctivas
+                   $accionesCorrectivas = \Illuminate\Support\Collection::make(\DB::table('accioncorrectiva1s')
+                   ->select('accioncorrectiva1s.*')
+                   ->where('accioncorrectiva1s.idcompañia','=',$usuarios->id_compania)
+                   ->where('accioncorrectiva1s.responsable_id','=',$usuarios->id)
+                   ->where('accioncorrectiva1s.estatus_id','!=',3)
+                   ->orwhere(function ($querys) use ($iduser) {
+                     $querys->where('accioncorrectiva1s.estatus_id','!=',3)
+                     ->where('accioncorrectiva1s.creador_id', '=', DB::raw("'".$iduser."'"));
+                   })
+                   ->get());
+                   //Final Acciones correctivas
+
+                   $Noconformidades = \Illuminate\Support\Collection::make(\DB::table('noconformidades')
+                   ->select('noconformidades.*')
+                   ->where('noconformidades.idcompañia','=',$usuarios->id_compania)
+                   ->where('noconformidades.usuario_responsable_id','=',$usuarios->id)
+                   ->where('noconformidades.estatus_id','!=',3)
+                   ->orwhere(function ($querys) use ($iduser) {
+                     $querys->where('noconformidades.estatus_id','!=',3)
+                     ->where('noconformidades.creador_id', '=', DB::raw("'".$iduser."'"));
+                   })
+                   ->get());
+
+
+		  }else{
 			     $mejorasid = \DB::table('mejoras')
                    ->join('users','mejoras.responsable_id','=','users.id')
-
                    ->leftjoin('lista_accesos','mejoras.listaequipo','=','lista_accesos.id_indicador')
                    ->select('mejoras.id', 'mejoras.proyecto')
                    ->where('mejoras.id_compania',$usuarios->id_compania)
+                   ->where('mejoras.estatus_id','!=',3)
                    ->groupby('mejoras.id')
                    ->get();
-		  }
-      $noticiasw = \DB::table('noticias')
-      ->join('lista_noticias', 'noticias.id', '=', 'lista_noticias.id_noticia')
-      ->select('noticias.*')
-      ->where('noticias.id_empresa',$usuarios->id_compania)
-      ->where('lista_noticias.id_area',$usuarios->id_area)
-      ->whereMonth('fecha_creacion', '=', date('m'))
-      //dd( $noticiasw);
-      ->get();
 
+                   $noticiasw = \DB::table('noticias')
+                   ->select('noticias.*')
+                   ->where('noticias.id_empresa',$usuarios->id_compania)
+                   ->whereMonth('fecha_creacion', '=', date('m'))
+                   ->get();
+
+                   $procesosa = new Proceso;
+                   $procesoa = $procesosa
+                   ->where('idcompañia',$usuarios->id_compania)
+                   ->get();
+
+                   $quejas = \Illuminate\Support\Collection::make(\DB::table('quejas')
+                   ->join('clientes','quejas.cliente_id','=','clientes.id')
+                   ->join('users','quejas.usuario_responsable_id','=','users.id')
+                   ->join('estatuses','quejas.estatus_id','=','estatuses.id')
+                   ->select('quejas.*','users.nombre as usernombre','clientes.nombre as clientenombre','estatuses.nombre as statusnombre')
+                   ->where('quejas.idcompañia','=',$usuarios->id_compania)
+                   ->where('quejas.estatus_id','!=',3)
+                   ->get());
+
+                   $accionesCorrectivas = \Illuminate\Support\Collection::make(\DB::table('accioncorrectiva1s')
+                   ->select('accioncorrectiva1s.*')
+                   ->where('accioncorrectiva1s.idcompañia','=',$usuarios->id_compania)
+                   ->where('accioncorrectiva1s.estatus_id','!=',3)
+                   ->get());
+
+                   $Noconformidades = \Illuminate\Support\Collection::make(\DB::table('noconformidades')
+                   ->select('noconformidades.*')
+                   ->where('noconformidades.idcompañia','=',$usuarios->id_compania)
+                   ->get());
+		  }
+
+      //Para los modales
+      $Users = new User;
+      $User = $Users->where('id_compania',$usuarios->id_compania)->get();
+
+      $Area = new Areas;
+      $Areas = $Area->where('id_compania',$usuarios->id_compania)->get();
+
+      $clientes = new Clientes;
+      $cliente = $clientes->where('id_compania',$usuarios->id_compania)->get();
+
+      $procesos = new Proceso;
+      $proceso = $procesos->where('idcompañia',$usuarios->id_compania)->get();
+
+      $indicador = \Illuminate\Support\Collection::make(\DB::table('indicadores')
+                               ->leftjoin('objetivos','indicadores.objetivo_id','=','objetivos.id')
+                               ->select('indicadores.*','objetivos.id_compania')
+                               ->where('objetivos.id_compania','=',$usuarios->id_compania)
+                               ->get());
+
+      //Sin necesidad de separar
       $Links = new LinksInteres;
       $Link = $Links->where('id_empresa',$usuarios->id_compania)->get();
 
@@ -145,41 +279,17 @@ class BienvenidaController extends Controller
       $documentos = new Documentos;
       $documento = $documentos ->where('id_user',$usuarios->id)->get();
 
+      $indicadores=new Indicadores;
+      $indicadora = $indicadores->where('usuario_responsable_id',$usuarios->id)->get();
+
       $pendientes = new Pendientes;
       $pendiente = $pendientes->where('id_UsuarioAsignado',$usuarios->id)->get();
-
-      $indicadores=new Indicadores;
-      $indicador = $indicadores->where('usuario_responsable_id',$usuarios->id)->get();
-
-      $objetivos = new Objetivo;
-      $objetivo = $objetivos->where('id_compania',$usuarios->id_compania)->get();
-
-      $queja = new Quejas;
-      $quejas = $queja->where('idcompañia',$usuarios->id_compania)->get();
-
-      $accionCorrectiva = new Accioncorrectiva1;
-      $accionesCorrectivas = $accionCorrectiva->where('idcompañia',$usuarios->id_compania)->get();
-
-      $Noconformidad = new Noconformidades;
-      $Noconformidades = $Noconformidad->where('idcompañia',$usuarios->id_compania)->get();
-
-      $Area = new Areas;
-      $Areas = $Area->where('id_compania',$usuarios->id_compania)->get();
-
-      $Users = new User;
-      $User = $Users->where('id_compania',$usuarios->id_compania)->get();
-
-      $procesos = new Proceso;
-      $proceso = $procesos->where('idcompañia',$usuarios->id_compania)->get();
-
-      $clientes = new Clientes;
-      $cliente = $clientes->where('id_compania',$usuarios->id_compania)->get();
 
       $proveedores = new proveedores;
       $proveedor = $proveedores->where('id_compania',$usuarios->id_compania)->orderBy('id')->get();
       $cuentaproveedor = $proveedor->count();
 
-
+      //Para super admin o partners
       if($usuarios->perfil == 1){
         $empresas = new Empresas;
         $empresa = $empresas->get();
@@ -191,13 +301,26 @@ class BienvenidaController extends Controller
       $events = [];
       $options = [];
 
-$allevents = \DB::table('event_models')
-      ->join('lista_eventos', 'event_models.id', '=', 'lista_eventos.id_evento')
-      ->select('event_models.*')
-      ->where('lista_eventos.id_area',$usuarios->id_area)
-      //->whereMonth('fecha_creacion', '=', date('m'))
-      //dd( $noticiasw);
-      ->get();
+      if($usuarios->perfil == 4)
+      {
+        $allevents = \DB::table('event_models')
+              ->join('lista_eventos', 'event_models.id', '=', 'lista_eventos.id_evento')
+              ->select('event_models.*')
+              ->where('lista_eventos.id_area',$usuarios->id_area)
+              //->whereMonth('fecha_creacion', '=', date('m'))
+              //dd( $noticiasw);
+              ->get();
+
+      }else{
+        $allevents = \DB::table('event_models')
+              ->join('lista_eventos', 'event_models.id', '=', 'lista_eventos.id_evento')
+              ->select('event_models.*')
+              ->where('lista_eventos.id_area',$usuarios->id_area)
+              //->whereMonth('fecha_creacion', '=', date('m'))
+              //dd( $noticiasw);
+              ->get();
+
+      }
 
 //return(dd($allevents));
 
@@ -253,12 +376,14 @@ $calendar = \Calendar::setCallbacks([
         'objetivo',
         'quejas',
         'proceso',
+        'procesoa',
         'cuentaproveedor',
         'empresa',
         'noticiasw',
         'calendar',
         'documento',
         'indicador',
+        'indicadora',
         'User',
         'pendiente',
         'accionesCorrectivas',
