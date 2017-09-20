1399a8927599e5421a1ddb8967face1c8a8e1ecb
Unknown <inf.jonathan.gomez@gmail.com>
2017-09-20 01:50:08 -0500

    Cambios para mostrar cambios de version pagina

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -209,11 +209,11 @@
         <div class="col-md-6">
           <button type="submit" class="btnobjetivo" id="btnEditCli" style="font-family: Arial;">Seleccionar</button>
       </div>
-        <button id="btn_modalversion" type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#modalversion" style="border:0px; width:0px; height:0px;"></button>
       </form>
     </div>
   </div>
 @endif
+<button id="btn_modalversion" type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#modalversion" style="border:0px; width:0px; height:0px;"></button>
 
   </div>
 </div>
@@ -553,39 +553,61 @@
         <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
-                <h3 class="modal-title">Nueva version</h3>
+              <center>
+                <h2 class="modal-title">ISObpm</h2>
+              </center>
             </div>
             <div class="modal-body">
               <form>
-                <div class="row">
+                <div class="row" style="height: 460px;">
                   <div id="pagina1">
                     <div class="col-lg-12 col-md-12 col-sm-12">
-                      <img src="/img/version/v1.png" style="width: 80%; height: 50%; float: left; margin-right:5px" />
+                      <p align="center" ><font size=6 color="#0167FF" >Nueva Versión</font></p>
                     </div>
                     <div class="col-lg-12 col-md-12 col-sm-12">
                       <p><font size=5>
-                        En este módulo de Oportunidades podrás generar tu catalogo de Oportunidades,
-                         y realizar la identificación y análisis de oportunidades de tus procesos
-                         desde su arquitectura. Además podrás acceder a un mapa de calor con la
-                         identificación y clasificación de las oportunidades detectadas en el análisis.
+                          En esta nueva versión de ISObpm encontrarás una imagen totalmente renovada tanto en su navegación como en contenido, así como Módulos adicionales que ayudarán en la Operación de tu Sistema de Gestión.
+                          <br>
+                          <br>
+                          ° Módulo de Inicio
+                          <br>
+                          ° Módulo de Proveedores
+                          <br>
+                          ° Módulo de Oportunidades
+                          <br>
                         </font>
                        </p>
                     </div>
                   </div>
                 <div id="pagina2" style="display:none;">
                   <div class="col-lg-12 col-md-12 col-sm-12">
-                    <h3><label> Decripcion2:</label></h3>
-                    <textarea class="form-control" id = "noticia__not" rows="3" name="noticia__not" placeholder="Descripcion noticia 2" required=""></textarea>
+                    <p align="center" ><font size=6 color="#0167FF" >Nueva Página de Inicio</font></p>
+                    <img src="/img/version/v2.png" style="width: 100%; height: 100%; float: left; margin-right:5px" />
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <p><font size=5>
+                      En esta página de inicio personalizada por usuario podrás identificar tu principal información del Sistema de Gestión, así como un nuevo calendario de actividades, Noticias, y Links
+                        <br>
+                      </font>
+                     </p>
                   </div>
                 </div>
                 <div id="pagina3" style="display:none;">
                   <div class="col-lg-12 col-md-12 col-sm-12">
-                    <h3><label> Decripcion3:</label></h3>
-                    <textarea class="form-control" id = "noticia__not" rows="3" name="noticia__not" placeholder="Descripcion noticia 3" required=""></textarea>
+                    <p align="center" ><font size=6 color="#0167FF" >Nuevo Módulo de Proveedores</font></p>
+                    <img src="/img/version/v3.png" style="width: 100%; height: 100%; float: left; margin-right:5px" />
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <p><font size=5>
+                      En este nuevo módulo de Proveedores podrás dar de alta tu catálogo de suministros, tu catalogo de proveedores, y podrás realizar la evaluación de estos proveedores. También tendrás acceso a un Dashboard de reportes de resultados de las evaluaciones a tus proveedores.
+                        <br>
+                      </font>
+                     </p>
                   </div>
                 </div>
                 <div id="pagina4" style="display:none;">
                   <div class="col-lg-12 col-md-12 col-sm-12">
+                    <p align="center" ><font size=6 color="#0167FF" >Nuevo Módulo de Oportunidades</font></p>
                     <img src="/img/version/v1.png" style="width: 80%; height: 50%; float: left; margin-right:5px" />
                   </div>
                   <div class="col-lg-12 col-md-12 col-sm-12">
@@ -598,6 +620,7 @@
                      </p>
                   </div>
                 </div>
+                </div>
                 <center>
                 <ul class="pagination">
                   <li id="lip1" class="active" ><a onClick="pagina1();">1</a></li>
@@ -606,7 +629,6 @@
                   <li id="lip4"><a onClick="pagina4();">4</a></li>
                 </ul>
                 </center>
-                </div>
               </form>
             </div>
           </div>
